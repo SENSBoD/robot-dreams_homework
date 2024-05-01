cd95e63aa1008498b58fa079c7af38fd2421e3eb
Bohdan Dolishnii <sensbod@gmail.com>
2024-05-01 22:02:48 +0300

    Add homework 3

--- lesson_07/dags/process_sales.py
+++ lesson_07/dags/process_sales.py
@@ -1,9 +1,10 @@
 from datetime import datetime
-
 from airflow import DAG
-from airflow.operators.bash import BashOperator
-from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
+from airflow.utils.trigger_rule import TriggerRule
+import os
+import requests
+
 
 DEFAULT_ARGS = {
     'depends_on_past': False,
@@ -14,31 +15,60 @@ DEFAULT_ARGS = {
     'retry_delay': 30,
 }
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.getcwd()))
 
-def some_task_function():
-    print('Hello from airflow!')
+JOB1_PORT = 8081
+JOB2_PORT = 8082
 
-def some_task_function2():
-    print('Hello from airflow!')
+RAW_DIR = os.path.join(BASE_DIR, "lesson_02", "raw", "sales", "2022-08-09")
+STG_DIR = os.path.join(BASE_DIR, "lesson_02", "stg", "sales", "2022-08-09")
+
+
+def run_job1():
+    print("Starting job1:")
+    resp = requests.post(
+        url=f'http://localhost:{JOB1_PORT}/',
+        json={
+            "date": "2022-08-09",
+            "raw_dir": RAW_DIR
+        }
+    )
+    assert resp.status_code == 201
+    print("job1 completed!")
+
+def run_job2():
+    print("Starting job2:")
+    resp = requests.post(
+        url=f'http://localhost:{JOB2_PORT}/',
+        json={
+            "raw_dir": RAW_DIR,
+            "stg_dir": STG_DIR
+        }
+    )
+    assert resp.status_code == 201
+    print("job2 completed!")
 
 dag = DAG(
     dag_id='process_sales',
-    start_date=datetime(2024, 4, 26),
+    start_date=datetime(2022, 8, 9),
+    end_date=datetime(2022, 8, 11),
     schedule_interval="0 1 * * *",
-    catchup=False,
+    catchup=True,
     default_args=DEFAULT_ARGS,
+    max_active_runs=1,
 )
 
 task1 = PythonOperator(
     task_id='extract_data_from_api',
     dag=dag,
-    python_callable=some_task_function
+    python_callable=run_job1,
+    trigger_rule=TriggerRule.ALL_SUCCESS
 )
 
 task2 = PythonOperator(
     task_id='convert_to_avro',
     dag=dag,
-    python_callable=some_task_function2
+    python_callable=run_job2
 )
 
 
